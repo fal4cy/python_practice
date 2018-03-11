06a3ee70b91a2de6bae370ec9586f1088687152e
fal4cy <36115764+fal4cy@users.noreply.github.com>
2018-03-11 18:06:52 -0400

    Update SEAL_COREY_M08_Midterm.py

--- SEAL_COREY_M08_Midterm.py
+++ SEAL_COREY_M08_Midterm.py
@@ -2,17 +2,17 @@
 # March 11th, 2018
 # Midterm
 
-def main():
-    first = input('Enter first word of sentence: ')
-    sentence = first[0].upper() + first[1:]
-    loop = 'y'
-    while loop.lower() == 'y':
-        new_input = input('Enter next word: ')
-        sentence += ' ' + new_input
-        loop = input('Continue? y/n: ')
-    sentence += '.'
-    print(sentence)
-    print('There are ', len(sentence.split()), ' words in the sentence.')
+def main():  # main module
+    first = input('Enter first word of sentence: ')  # get initial input
+    sentence = first[0].upper() + first[1:]  # caps the first letter in the string
+    loop = 'y'  # initialize loop variable
+    while loop.lower() == 'y':  # checks status of loop variable
+        new_input = input('Enter next word: ')  # gets next word in string
+        sentence += ' ' + new_input  # concatenated it into sentence string
+        loop = input('Continue? y/n: ')  # ask for loop variable
+    sentence += '.'  # adds period to end
+    print(sentence)  # prints sentence
+    print('There are ', len(sentence.split()), ' words in the sentence.')  # prints length of str
 
-# call main
+# call main function
 main()
